c45aa7ebb2cebd9e946c41ec006b6e26f685b7f1
Silverlan <silverlann@gmail.com>
2023-06-15 13:19:29 +0200

    fix(windows): fix sub-process creating new windows if pragma console is not open

--- subprocess/src/main.cpp
+++ subprocess/src/main.cpp
@@ -3,19 +3,16 @@
 
 // See https://bitbucket.org/chromiumembedded/cef/wiki/GeneralUsage#markdown-header-separate-sub-process-executable
 
-
 #ifdef __linux__
 #define DLL_PR_CHROMIUM __attribute__((visibility("default")))
 #else
 #define DLL_PR_CHROMIUM __declspec(dllexport)
+
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 
-extern "C"
-{
-    DLL_PR_CHROMIUM bool pr_chromium_subprocess(int,char**);
+extern "C" {
+DLL_PR_CHROMIUM bool pr_chromium_subprocess(int, char **);
 };
 
-int main(int argc,char *argv[])
-{
-    return pr_chromium_subprocess(argc,argv) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
+int main(int argc, char *argv[]) { return pr_chromium_subprocess(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE; }
